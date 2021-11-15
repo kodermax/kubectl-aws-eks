20c92656c1bf1a633a10ebe1407105809b99bea3
Zdenek Hasek <me@zdehasek.com>
2021-11-15 16:47:35 +0100

    AWS credentials fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
+mkdir -p ~/.aws
+printf "[$AWS_PROFILE]\naws_access_key_id = ${AWS_ACCESS_KEY_ID}\naws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}\n" > ~/.aws/credentials
+
 sh -c "kubectl $*"
