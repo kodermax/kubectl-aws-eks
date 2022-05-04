5763c29422f2786af438f97d11b6181be18c3fb1
Mark D. Gray <markdaltongray@gmail.com>
2022-05-04 06:55:09 -0500

    Modifications to versioning and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM amazon/aws-cli
+FROM amazon/aws-cli:latest
 
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
-    curl -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator && \
+RUN yum install jq -y && curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
+    curl -sL -o /usr/local/bin/aws-iam-authenticator $(curl -s https://api.github.com/repos/kubernetes-sigs/aws-iam-authenticator/releases/latest | jq -r ' .assets[] | select(.name | contains("linux_amd64")    )' | jq -r '.browser_download_url')  && \
     chmod +x /usr/local/bin/aws-iam-authenticator && \
-    chmod +x ./kubectl && \
-    mv ./kubectl /usr/bin/kubectl 
+    chmod +x /usr/bin/kubectl
+    #mv ./kubectl /usr/bin/kubectl
 
-RUN kubectl version --client
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,25 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
+
 if [ -z ${KUBECTL_VERSION+x} ] ; then
-    echo "kubectl version: $(kubectl version --client --short)"
+    echo "Using kubectl version: $(kubectl version --client --short)"
 else
-    echo "Repulling kubectl for version $KUBECTL_VERSION"
+    echo "Pulling kubectl for version $KUBECTL_VERSION"
     rm /usr/bin/kubectl 
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && \
-        chmod +x kubectl && \
-        mv kubectl /usr/local/bin/
-    echo "kubectl version: $(kubectl version --client --short)"
+    curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && \
+        chmod +x /usr/bin/kubectl
+    echo "Using kubectl version: $(kubectl version --client --short)"
 fi
+
+if [ -z ${IAM_VERSION+x} ] ; then
+    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version)"
+else
+    echo "Pulling aws-iam-authenticator for version $IAM_VERSION"
+    rm /usr/bin/aws-iam-authenticator
+    curl -sL -o /usr/local/bin/aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v$IAM_VERSION/aws-iam-authenticator_$IAM_VERSION_linux_amd64 && \
+        chmod +x /usr/local/bin/aws-iam-authenticator
+    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version)"
+fi
+
 sh -c "kubectl $*"
