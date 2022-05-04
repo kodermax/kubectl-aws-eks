07bc6868fcc5e50dc2ea684daf4ecd39904f020f
Mark D. Gray <markdaltongray@gmail.com>
2022-05-04 07:30:03 -0500

    quotes added to IAM_VERSION var

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,20 +10,21 @@ if [ -z ${KUBECTL_VERSION+x} ] ; then
     echo "Using kubectl version: $(kubectl version --client --short)"
 else
     echo "Pulling kubectl for version $KUBECTL_VERSION"
-    rm /usr/bin/kubectl 
-    curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && \
+    rm /usr/bin/kubectl
+    curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl && \
         chmod +x /usr/bin/kubectl
     echo "Using kubectl version: $(kubectl version --client --short)"
 fi
 
 if [ -z ${IAM_VERSION+x} ] ; then
-    echo "Using aws-iam-authenticator version: v$(aws-iam-authenticator version)"
+    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version)"
 else
-    echo "Pulling aws-iam-authenticator for version v$IAM_VERSION"
-    rm /usr/local/bin/aws-iam-authenticator
-    curl -sL -o /usr/local/bin/aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v$IAM_VERSION/aws-iam-authenticator_$IAM_VERSION_linux_amd64 && \
-        chmod +x /usr/local/bin/aws-iam-authenticator
-    echo "Using aws-iam-authenticator version: v$(aws-iam-authenticator version)"
+    echo "Pulling aws-iam-authenticator for version $IAM_VERSION"
+    rm /usr/bin/aws-iam-authenticator
+    curl -sL -o /usr/bin/aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v"$IAM_VERSION"/aws-iam-authenticator_"$IAM_VERSION"_linux_amd64 && \
+    chmod +x /usr/bin/aws-iam-authenticator
+    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version)"
 fi
 
 sh -c "kubectl $*"
+#sh -c "which aws-iam-authenticator"
