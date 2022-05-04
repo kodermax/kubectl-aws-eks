cb6e5e539ac2a0a3df74f523ade6c97622472b61
Mark D. Gray <markdaltongray@gmail.com>
2022-05-03 19:41:30 -0500

    Patch-2 for kubectl version ENV

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM amazon/aws-cli
 
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
     curl -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator && \
     chmod +x /usr/local/bin/aws-iam-authenticator && \
     chmod +x ./kubectl && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,14 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
-
+if [ -z ${KUBECTL_VERSION+x} ] ; then
+    echo "kubectl version: $(kubectl version --client --short)"
+else
+    echo "Repulling kubectl for version $KUBECTL_VERSION"
+    rm /usr/bin/kubectl 
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && \
+        chmod +x kubectl && \
+        mv kubectl /usr/local/bin/
+    echo "kubectl version: $(kubectl version --client --short)"
+fi
 sh -c "kubectl $*"
