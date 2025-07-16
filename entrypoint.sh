2cb35bec6ba8139ec3bd197b736d8dec7e1a4137
patrickdk <patrickdk@patrickdk.com>
2025-07-16 10:50:43 -0400

    add arch support to entrypoint do not download version if already installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,25 +18,29 @@ if [ ! -s /tmp/config ]; then
     exit 1
 fi
 
-if [ -z ${KUBECTL_VERSION+x} ] ; then
-    echo "Using kubectl version: $(kubectl version --client 2>&1)"
-else
-    echo "Pulling kubectl for version $KUBECTL_VERSION"
-    rm -f /usr/bin/kubectl
-    curl -sL -o /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl && \
-        chmod +x /usr/bin/kubectl
-    echo "Using kubectl version: $(kubectl version --client --short 2>&1)"
+if [ ! -z "${KUBECTL_VERSION}" ]; then
+    if [ ! -e /usr/bin/kubectl-${KUBECTL_VERSION} ]; then
+        echo "Pulling kubectl for version $KUBECTL_VERSION"
+        curl -sL -o /usr/bin/kubectl-${KUBECTL_VERSION} https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/$TARGETARCH/kubectl && \
+            chmod +x /usr/bin/kubectl-${KUBECTL_VERSION} && \
+            ln -f -s /usr/bin/kubectl-${KUBECTL_VERSION} /usr/bin/kubectl
+    else
+      ln -f -s /usr/bin/kubectl-${KUBECTL_VERSION} /usr/bin/kubectl
+    fi
 fi
+echo "Using kubectl version: $(kubectl version --client 2>&1)"
 
-if [ -z ${IAM_VERSION+x} ] ; then
-    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version 2>&1)"
-else
-    echo "Pulling aws-iam-authenticator for version $IAM_VERSION"
-    rm -f /usr/bin/aws-iam-authenticator
-    curl -sL -o /usr/bin/aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v"$IAM_VERSION"/aws-iam-authenticator_"$IAM_VERSION"_linux_amd64 && \
-    chmod +x /usr/bin/aws-iam-authenticator
-    echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version 2>&1)"
+if [ ! -z "${IAM_VERSION}" ]; then
+    if [ ! -e /usr/bin/aws-iam-authenticator-${IAM_VERSION} ]; then
+        echo "Pulling aws-iam-authenticator for version $IAM_VERSION"
+        curl -sL -o /usr/bin/aws-iam-authenticator-${IAM_VERSION} https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v"$IAM_VERSION"/aws-iam-authenticator_"$IAM_VERSION"_linux_$TARGETARCH && \
+            chmod +x /usr/bin/aws-iam-authenticator-${IAM_VERSION} && \
+            ln -f -s /usr/bin/aws-iam-authenticator-${IAM_VERSION} /usr/bin/aws-iam-authenticator
+    else
+        ln -f -s /usr/bin/aws-iam-authenticator-${IAM_VERSION} /usr/bin/aws-iam-authenticator
+    fi
 fi
+echo "Using aws-iam-authenticator version: $(aws-iam-authenticator version 2>&1)"
 
 # Check if tools were successfully installed
 if ! command -v kubectl >/dev/null 2>&1; then
